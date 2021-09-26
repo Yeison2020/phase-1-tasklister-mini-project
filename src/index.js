8fdb94a51567c46290aff7fc57fc70e3e365272c
Yeison <enmanuelquiterios@gmail.com>
2021-09-26 18:29:36 -0400

    TODO-List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    builderText(e.target.newtaskdescription.value);
+    form.reset();
+  });
 });
+
+function builderText(data) {
+  if (data !== "") {
+    let p = document.createElement("p");
+    let btn = document.createElement("buttom");
+    btn.addEventListener("click", deleteButtom);
+    btn.textContent = "  X";
+    p.append(`${data}`);
+    p.appendChild(btn);
+    document.querySelector("#tasks").appendChild(p);
+  } else {
+    return;
+  }
+}
+
+function deleteButtom(e) {
+  e.target.parentNode.remove();
+}
