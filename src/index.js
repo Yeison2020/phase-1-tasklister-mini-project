3c1339d60a4bbacc4a456fadc4dbb13e05ef9528
Yeison <enmanuelquiterios@gmail.com>
2021-10-07 12:24:10 -0400

    Verifying Improvements

--- src/index.js
+++ src/index.js
@@ -1,51 +1,51 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   let form = document.querySelector("form");
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     builderText(e.target.newtaskdescription.value);
-//     form.reset();
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    builderText(e.target.newtaskdescription.value);
+    form.reset();
+  });
+});
 
-// function builderText(data) {
-//   if (data !== "") {
-//     let p = document.createElement("p");
-//     let btn = document.createElement("buttom");
-//     btn.addEventListener("click", deleteButtom);
-//     btn.textContent = "  X";
-//     p.append(`${data}`);
-//     p.appendChild(btn);
-//     document.querySelector("#tasks").appendChild(p);
-//   } else {
-//     return;
-//   }
-// }
+function builderText(data) {
+  if (data !== "") {
+    let p = document.createElement("p");
+    let btn = document.createElement("buttom");
+    btn.addEventListener("click", deleteButtom);
+    btn.textContent = "  X";
+    p.append(`${data}`);
+    p.appendChild(btn);
+    document.querySelector("#tasks").appendChild(p);
+  } else {
+    return;
+  }
+}
 
-// function deleteButtom(e) {
-//   e.target.parentNode.remove();
-// }
+function deleteButtom(e) {
+  e.target.parentNode.remove();
+}
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   let form = document.querySelector("form");
-//   form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     textBuilder(e.target.newtaskdescription.value);
-//     form.reset();
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    textBuilder(e.target.newtaskdescription.value);
+    form.reset();
+  });
+});
 
-// function textBuilder(data) {
-//   let p = document.createElement("p");
-//   let btn = document.createElement("buttom");
-//   btn.addEventListener("click", deleter);
-//   p.append(`${data}`);
-//   p.appendChild(btn);
-//   document.querySelector("#tasks").appendChild(p);
-// }
+function textBuilder(data) {
+  let p = document.createElement("p");
+  let btn = document.createElement("buttom");
+  btn.addEventListener("click", deleter);
+  p.append(`${data}`);
+  p.appendChild(btn);
+  document.querySelector("#tasks").appendChild(p);
+}
 
-// function deleter(e) {
-//   e.target.parentNode.remove();
-// }
+function deleter(e) {
+  e.target.parentNode.remove();
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   let form = querySelector("form");
